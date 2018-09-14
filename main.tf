0eb4e3bc4bd3f33af41be6868e3bebb1cd3cd008
Teerasak Vichadee <teerasak.vic@ascendcorp.com>
2018-09-14 15:51:22 +0700

    REMOVE module and use registered module instead

--- main.tf
+++ main.tf
@@ -2,11 +2,55 @@ provider "aws" {
   region = "${var.region}"
 }
 
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnet_ids" "all" {
+  vpc_id = "${data.aws_vpc.default.id}"
+}
+
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+
+  filter {
+    name = "name"
+    values = [
+      "amzn-ami-hvm-*-x86_64-gp2",
+    ]
+  }
+
+  filter {
+    name = "owner-alias"
+    values = [
+      "amazon",
+    ]
+  }
+}
+
+module "security_group" {
+  source = "terraform-aws-modules/security-group/aws"
+
+  name        = "trf48-sg"
+  vpc_id      = "${data.aws_vpc.default.id}"
+
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  ingress_rules       = ["http-80-tcp", "all-icmp"]
+  egress_rules        = ["all-all"]
+}
+
+resource "aws_eip" "this" {
+  vpc      = true
+  instance = "${module.ec2.id[0]}"
+}
+
 module "ec2" {
-  source = "modules/ec2"
-  project = "${var.project}"
-  platform = "${terraform.workspace}"
-  team = "${var.team}"
-  ami = "${lookup(var.ami, var.region, "ami-08569b978cc4dfa10")}"
+  source = "terraform-aws-modules/ec2-instance/aws"
+
+  name = "trf48-cluster"
+  ami = "${data.aws_ami.amazon_linux.id}"
   instance_type = "${lookup(var.instance_type, "amazon_linux_ami", "t2.micro")}"
+  subnet_id = "${element(data.aws_subnet_ids.all.ids, 0)}"
+  vpc_security_group_ids = ["${module.security_group.this_security_group_id}"]
+  associate_public_ip_address = true
 }
